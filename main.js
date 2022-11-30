8beb6888602a444b7336a2cf8c1a3c2983178d6a
HuiKaiChung <hakuya8zai@gmail.com>
2022-11-30 16:34:21 +0800

    button -> radio button with value

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $('#header').load('header.html');
 $('#footer').load('footer.html');
 console.log("mainjs")
+
+let a = $('#option1').val();
+let b = $('input[name="options"]');
+console.log(b);
+console.log(a);
+
+$('input[name="options"]').click(function(){
+    totalRecal($(this).val());
+});
+
+function totalRecal(newCost){
+    console.log(newCost);
+$('#total-cost').text(newCost);
+}
