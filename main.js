a586643e50e98834b35d5b146d263c11c9f1bcfc
HuiKaiChung <hakuya8zai@gmail.com>
2022-12-02 11:24:44 +0800

    add template in cart

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-$('#header').load('header.html');
+$('#header').load('header.html', function(){
+    $('#add-items').click(addItem);
+});
 $('#footer').load('footer.html');
-console.log("mainjs")
 
-let a = $('#option1').val();
-let b = $('input[name="options"]');
-console.log(b);
-console.log(a);
 
-$('input[name="options"]').click(function(){
-    totalRecal($(this).val());
+$('input[name="definition-options"]').click(function(){
+    totalRecal(
+        $(this).val(),
+        $('input[name="time-range-options"]:checked').val()
+        );
+});
+$('input[name="time-range-options"]').click(function(){
+    totalRecal(
+        $('input[name="definition-options"]:checked').val(),
+        $(this).val()
+        );
 });
 
-function totalRecal(newCost){
+
+function totalRecal(newCost, newTimeRange){
     console.log(newCost);
-$('#total-cost').text(newCost);
+    console.log(newTimeRange);
+    let FinalCost = newCost*newTimeRange;
+    console.log(FinalCost);
+    $('#total-cost').text(FinalCost);
+}
+
+
+
+//To do : 好像可以把參數們包成物件丟過來
+function addItem(){
+    let newItem = '<li><a class="dropdown-item" href="#"><span><span><img src="./Image/One.svg" alt="" /></span><span>Item Name and Sth Else like great</span></span></a></li>';
+    $('#big-cart-list').prepend(newItem);
+    $('#small-cart-list').prepend(newItem);
+    
+
 }
