cc419e680be6b0bc83df737331597d31d8fda730
HuiKaiChung <hakuya8zai@gmail.com>
2023-01-09 15:05:13 +0800

    date picker lock complete

--- contact.js
+++ contact.js
@@ -22,4 +22,34 @@
   });
 })();
 
-console.log(new Date());
+setStartDate();
+setEndDate();
+// 得到今天的日子，並讓開始日不能在此之前
+function setStartDate() {
+  const Today = new Date();
+  let yyyy = Today.getFullYear();
+  let mm = Today.getMonth();
+  let dd = Today.getDate();
+  mm += 1;
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+  let minDate = yyyy + "-" + mm + "-" + dd;
+  const startPicker = document.getElementById("startDate");
+  startPicker.setAttribute("min", minDate);
+}
+function setEndDate() {
+  const startPicker = document.getElementById("startDate");
+  const endPicker = document.getElementById("endDate");
+  endPicker.setAttribute("disabled", true);
+  startPicker.addEventListener("change", () => {
+    if (startPicker.value != "") {
+      endPicker.removeAttribute("disabled");
+    }
+    endPicker.setAttribute("min", startPicker.value);
+    //TO DO : 照理來說還要做一個檢查 end date 是不是晚於 start date
+  });
+}
